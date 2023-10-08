abaa9a3378ec414dd0bb4635e76d7b75e3d1a4ba
Ricky Stroud <stroudrx1@gmail.com>
2023-10-08 15:33:49 +0000

    loops

---
+++ module3-for-loop.py
@@ -0,0 +1,7 @@
+# for loop[
+for letter in 'hello!':
+    print('Current letter', letter)
+
+for counter in range(1, 11, 4):
+    print(counter)
+print('Finished! ')
---
+++ module3-while-loop.py
@@ -0,0 +1,24 @@
+# 2 types of loops
+# while
+# for
+#vwhile condition:
+#    do_something()
+#    do_something2()
+
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1
+print('Finished! ')
+
+secret_number = 14
+print('''
+==========================
+=== SECRET NUMBER GAME ===
+==========================
+''')
+user_input = int(input('Try to guess the secret number from 0 to 20: '))
+while user_input != secret_number:
+    print('Wrong!')
+    user_input = int(input('Try to guess the secret number from 0 to 20: '))
+print('Perfect! You have guessed the secret number. ')
